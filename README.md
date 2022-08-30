5a47829c1a15515150a8bb357e9688fe80708720
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-08-30 16:10:05 +0100

    adds ci using github actions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Test-Driven Development with FastAPI and Docker
+
+![Continuous Integration and Delivery](https://github.com/BigTava/tdd-fastapi-docker-setup/workflows/Continuous%20Integration%20and%20Delivery/badge.svg?branch=main)
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,11 +1,12 @@
 import os
+
 import pytest
 from starlette.testclient import TestClient
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.main import create_application
 from app import main
-from app.config import get_settings, Settings
+from app.config import Settings, get_settings
+from app.main import create_application
 
 
 def get_settings_override():
